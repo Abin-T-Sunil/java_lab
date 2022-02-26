512ee0e352e71cbbadcb5aeee3650791cd214d18
Abin T Sunil <80634744+Abin-T-Sunil@users.noreply.github.com>
2022-02-26 20:14:09 +0530

    added exp 3

---
+++ Java Lab S3/Cycle4/garbage.java
@@ -0,0 +1,15 @@
+public class garbage 
+{
+    String name;
+    public static void main(String[] args) 
+    {
+        Experiment3 e=new Experiment3();
+        e.name="Karthik";
+        e=null;
+        System.gc();
+    }
+    public void finalize()
+    {
+        System.out.println("Onject is garbage collected");
+    }
+}
